b587e60726bf94edc1b3998a98dd84eefe997769
Murilo Viscondi <50927186+muriloviscondi@users.noreply.github.com>
2019-12-10 07:16:04 -0300

    Update script.js

--- Periodo do Dia/js/script.js
+++ Periodo do Dia/js/script.js
@@ -1,21 +1,23 @@
-alert('fucinona')
-
 function carregar() {
     
     var tempo = document.getElementById('tempo');
     var img = document.getElementById('image')
     var horario = new Date;
 
-    hora =  13 //horario.getHours();
+    hora =  horario.getHours();
     minutos = horario.getMinutes();
+    
     tempo.innerHTML = (`${hora}:${minutos}`);
 
     if (hora < 12) {
-        img.src = 'C:\xampp\htdocs\Curso em Video\JavaScript\ex013aula12\img\manha.png';
+        img.src = '..\img\manha.png';
+        document.body.style.backgroundColor = '#b3e6ff';
     } else if (hora < 18) {
-        img.src = 'C:\xampp\htdocs\Curso em Video\JavaScript\ex013aula12\img\tarde.png';
+        img.src = '..\img\tarde.png';
+        document.body.style.backgroundColor = '#d9ffb3';
     } else {
-        img.src = 'C:\xampp\htdocs\Curso em Video\JavaScript\ex013aula12\img\noite.png'
+        img.src = '..\img\noite.png'
+        document.body.style.backgroundColor = '#666666';
     }
 
 }

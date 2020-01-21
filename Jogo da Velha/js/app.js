9b5135e4023b841826da1e8af9b912bed0ad139d
Murilo Viscondi <50927186+muriloviscondi@users.noreply.github.com>
2020-01-21 07:46:46 -0300

    Delete app.js

--- Jogo da Velha/js/app.js
+++
@@ -1,7 +0,0 @@
-function teste() {
-   let teste1 = document.querySelector('#position-00-00');
-   let myImage = new Image;
-   myImage.src = 'http://img1.wikia.nocookie.net/__cb20110929163657/simpsons/images/d/db/200px-Chuck_Muntz.png';
-   teste1.append(myImage);
-    
-}
